ed413919306a0d1e900a02e03d80f0bca1f67595
grossteddy <teddy.kidi@gmail.com>
2025-02-18 20:17:24 +0000

    Added Camera follow lerp

---
+++ Assets/0. Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Settings")]
+    [Range(0.01f,10f)][SerializeField] private float _lerpTime = 5f;
+    private float _timePassed;
+ 
+    [Header("References")]
+    [SerializeField] private Transform _playerPosition; 
+
+
+    void FixedUpdate()
+    {
+        LerpFollowPlayer();
+    }
+
+    public void LerpFollowPlayer()
+    {
+        Vector3 temp = transform.position;
+        temp.x = Mathf.Lerp(transform.position.x, _playerPosition.position.x, Time.deltaTime * _lerpTime);
+        temp.y = Mathf.Lerp(transform.position.y, _playerPosition.position.y, Time.deltaTime * _lerpTime);
+        transform.position = temp;
+    }                   
+}
