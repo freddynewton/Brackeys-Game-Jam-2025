ebfcb939336e794582b8440714c5eead6a4cc7b5
Freddy Newton <freddakdogan2@gmail.com>
2025-02-19 14:13:39 +0100

    Add Janitor Hand System

--- Assets/ItemHolderLerpPosition.cs
+++ Assets/ItemHolderLerpPosition.cs
@@ -1,8 +1,11 @@
+using UnityEditor;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemHolderLerpPosition : MonoBehaviour
 {
     [SerializeField] private Transform _targetTransform;
+    [SerializeField] private Transform _playerGraphics;
     [SerializeField] private float _lerpSpeed = 15f;
     [SerializeField] private float _campRange = 0.2f;
 
@@ -10,16 +13,26 @@ public class ItemHolderLerpPosition : MonoBehaviour
     {
         // Set the parent transform to null so we can move the item holder freely
         transform.parent = null;
+
+        _targetTransform.DOLocalMoveY(_targetTransform.localPosition.y + 0.05f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutBack);
     }
 
     private void Update()
     {
         // Calculate the direction from the item holder to the target transform
-        Vector2 lerpDirection = _targetTransform.position - transform.position.normalized;
+        Vector3 lerpDirection = _targetTransform.position - transform.position;
 
-        // Lerp the position of the item holder to the target transform
-        // the target is our parent transform
-        // and clamp the range of the item holder to the target transform
-        transform.position = Vector3.Lerp(transform.position, lerpDirection * _campRange, _lerpSpeed * Time.deltaTime);
+        // Lerp the position of the item holder to the target transform on the x-axis only
+        Vector3 newPosition = _targetTransform.position;
+        newPosition.x = Mathf.Lerp(transform.position.x, _targetTransform.position.x, _lerpSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
+        if (Mathf.Abs(lerpDirection.x) >= _campRange)
+        {
+            newPosition.x = _targetTransform.position.x - (Mathf.Sign(lerpDirection.x) * _campRange);
+            transform.position = newPosition;
+        }
+
+        transform.localScale = _playerGraphics.localScale;
     }
 }
