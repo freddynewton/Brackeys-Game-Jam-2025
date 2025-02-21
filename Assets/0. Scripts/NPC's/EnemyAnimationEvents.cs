3aede8aaadb0eca53cc6249fe3cfe0d346d557d8
grossteddy <teddy.kidi@gmail.com>
2025-02-21 00:26:41 +0000

    Added VFX and puker enemy type

--- Assets/0. Scripts/NPC's/EnemyAnimationEvents.cs
+++ Assets/0. Scripts/NPC's/EnemyAnimationEvents.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class EnemyAnimationEvents : MonoBehaviour
 {
     [SerializeField] private IDamageable _damageable;
+    [SerializeField] private Transform _damagePosition;
     [SerializeField] private int _damage = 1;
+    [SerializeField] private GameObject _vFX;
     private bool _isInAnimation = false;
 
     private void Awake()
     {
         _damage = gameObject.GetComponentInParent<EnemyInformation>().GetEnemyDamagePerAttack();
-        SetDamageable(GameObject.FindGameObjectWithTag("Player").GetComponent<UnitStatHandler>());
+        if(_damagePosition == null)
+        {
+            _damagePosition = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        _damageable = GameObject.FindGameObjectWithTag("Player").GetComponent<UnitStatHandler>();
     }
 
 
@@ -19,12 +25,20 @@ public class EnemyAnimationEvents : MonoBehaviour
         _damageable.TakeDamage(_damage, transform.position);
     }
 
-    public void SetDamageable(IDamageable damageable)
+    public void SetDamageable(GameObject damageable)
     {
-        _damageable = damageable;
+        Component[] components = gameObject.GetComponents(typeof(Component));
+        foreach (Component component in components)
+        {
+            if(component is IDamageable)
+            {
+                _damageable = (IDamageable)component;
+                _damagePosition = component.transform;
+            }
+        }
     }
 
-    public void SetAndHit(IDamageable damageable)
+    public void SetAndHit(GameObject damageable)
     {
         SetDamageable(damageable);
         Hit();
@@ -37,6 +51,14 @@ public class EnemyAnimationEvents : MonoBehaviour
     public void SetIsInAnimation()
     {
         _isInAnimation = !_isInAnimation;
+    }
+
+    public void DeployVFXOnTarget()
+    {
+        if(_damagePosition != null)
+        {
+            Instantiate(_vFX, _damagePosition);
+        }
     }
 
 }
