d78f20465f03167e519c298a4d936379bc47d96f
grossteddy <teddy.kidi@gmail.com>
2025-02-21 14:56:07 +0000

    someRandomshit

---
+++ Assets/0. Scripts/UnitStateBasedStatHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStateBasedStatHandler : UnitStatHandler
+{
+    [Header("State Based Settings")]
+    [SerializeField] private List<StatState> _states = new List<StatState>();
+
+    protected override void Death()
+    {
+    }
+
+    public override void TakeDamage(int damage, Vector3 attackerTransformPosition)
+    {
+        if (_currentHp <= 0)
+        {
+            return;
+        }
+
+        StartFlickering();
+
+        PlayVfx(_hitVfx, attackerTransformPosition);
+
+        _currentHp -= 1;
+
+        if (_states.Exists(state => state.Health == _currentHp))
+        {
+            UpdateSprite();
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        // Get Sprite based on current health based on the Health property of the StatState using Linq
+        Sprite sprite = _states.Find(state => state.Health == _currentHp).Sprite;
+
+        if (sprite != null)
+        {
+            _spriteRenderer.sprite = sprite;
+        }
+    }
+}
+
+[Serializable]
+public class StatState
+{
+    public int Health;
+
+    public Sprite Sprite;
+}
