ef43236cc1cf3e858cae4c5f52adc65542a8e28f
Freddy Newton <freddakdogan2@gmail.com>
2025-02-22 17:35:56 +0100

    Menu

---
+++ Assets/0. Scripts/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [Range(0,100)][SerializeField] private int scene = 1;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
