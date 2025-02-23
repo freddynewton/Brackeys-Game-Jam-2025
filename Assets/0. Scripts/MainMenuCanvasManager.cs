f2d0f991f91d95f038ea07fc87ccdbb882a85532
grossteddy <teddy.kidi@gmail.com>
2025-02-23 08:54:14 +0000

    dfbdfb

--- Assets/0. Scripts/MainMenuCanvasManager.cs
+++ Assets/0. Scripts/MainMenuCanvasManager.cs
@@ -9,12 +9,21 @@ public class MainMenuCanvasManager : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _settingsButton;
     [SerializeField] private Button _quitButton;
+    [SerializeField] private Button _backButton;
+
+    [SerializeField] private Slider _optionsSlider;
+
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _optionsMenu;
 
     private void Start()
     {
         _playButton.onClick.AddListener(OnPlayButtonClicked);
         _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
         _quitButton.onClick.AddListener(OnQuitButtonClicked);
+        _backButton.onClick.AddListener(OnBackbuttonPressed);
+
+        //_optionsSlider.onValueChanged(SoundManager.Instance.);
     }
 
     private void Awake()
@@ -23,11 +32,13 @@ public class MainMenuCanvasManager : MonoBehaviour
         _playButton.onClick.RemoveAllListeners();
         _settingsButton.onClick.RemoveAllListeners();
         _quitButton.onClick.RemoveAllListeners();
+        _backButton.onClick.RemoveAllListeners();
 
         // add new click listeners
         _playButton.onClick.AddListener(OnPlayButtonClicked);
         _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
         _quitButton.onClick.AddListener(OnQuitButtonClicked);
+        _backButton.onClick.AddListener(OnBackbuttonPressed);
 
         // set the position of the Barry portrait with DoTween up and down
         _barryPortrait.DOAnchorPosY(-50f, 3f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
@@ -40,11 +51,19 @@ public class MainMenuCanvasManager : MonoBehaviour
 
     private void OnSettingsButtonClicked()
     {
-        Debug.Log("Settings button clicked");
+        _optionsMenu.SetActive(true);
+        _mainMenu.SetActive(false);
     }
 
     private void OnPlayButtonClicked()
     {
         GameSceneFlowManager.Instance.LoadScene("Level 0", true);
     }
+
+    private void OnBackbuttonPressed()
+    {
+        _optionsMenu.SetActive(false);
+        _mainMenu.SetActive(true);
+    }
+
 }
